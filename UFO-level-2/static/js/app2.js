a3fe4f2fbab1c4a60ad3e027b0a1161d62ffe6d3
Ivan Velazquez <Ivanmichel@MacBook-Pro-de-Ivan-6.local>
2020-08-18 11:43:42 -0500

    Function level 2 done

--- UFO-level-2/static/js/app2.js
+++ UFO-level-2/static/js/app2.js
@@ -1,11 +1,12 @@
 let tableData = data;
+let tbody = d3.select("tbody");
+
 /**
  * Function to show initial data from the object
  * Append rows and data
  * @param {array} array 
  */
 function table(array) {
-    let tbody = d3.select("tbody");
     array.forEach(function (sighteen) {
         //console.log(sighteen);
         let row = tbody.append("tr");
@@ -15,9 +16,11 @@ function table(array) {
             cell.text(value);
         });
     });
-
+    
 }
 
+// Keep Track of all filters
+var filters = {};
 
 /**
  * Clear table body of initial data
@@ -40,33 +43,33 @@ function clearInputs() {
     // Call function to clear body and display initial data
     clearBody();
     table(tableData);
+    filters = {}
+    
 }
 
-
-// Keep Track of all filters
-var filters = {};
 
 function updateFilters() {
     
     // Save the element, value, and id of the filter that was changed
-    var changedElement = d3.select(this).select("input");
-    console.log(changedElement)
-    var elementValue = changedElement.property("value");
-    var filterId = changedElement.attr("id");
-    
-    
-    // If a filter value was entered then add that filterId and value
-    // to the filters list. Otherwise, clear that filter from the filters object
-    if (elementValue) {
-        filters[filterId] = elementValue;
-    }
-    else {
-        delete filters[filterId];
-    }
-    
-    // Call function to apply all filters and rebuild the table
-    filterTable();
-    
+  var changedElement = d3.select(this).select("input");
+  var elementValue = changedElement.property("value");
+  var filterId = changedElement.attr("id");
+
+  // If a filter value was entered then add that filterId and value
+  // to the filters list. Otherwise, clear that filter from the filters object
+  if (elementValue) {
+    filters[filterId] = elementValue;
+  }
+  else {
+    delete filters[filterId];
+  }
+
+  clearBody();
+  console.log(filters)
+  // Call function to apply all filters and rebuild the table
+  filterTable();
+
+
 }
 
 console.log(filters)
@@ -74,49 +77,27 @@ console.log(filters)
 function filterTable() {
     
     // Set the filteredData to the tableData
-    let filteredData = tableData;
-    
-    // Loop through all of the filters and keep any data that
-    // matches the filter values
-    Object.entries(filters).forEach(([key, value]) => {
-        filteredData = filteredData.filter(row => row[key] === value);
-    });
-    
-    // Finally, rebuild the table using the filtered Data
-    table(filteredData);
+  let filteredData = tableData;
+
+  // Loop through all of the filters and keep any data that
+  // matches the filter values
+  Object.entries(filters).forEach(([key, value]) => {
+    filteredData = filteredData.filter(row => row[key] === value);
+  });
+
+  console.log(filteredData)
+  // Finally, rebuild the table using the filtered Data
+  table(filteredData);
+
 }
 
 //Show data
 table(tableData);
 
 // D3 to select buttons and form
-d3.select("#button").on("click", filterTable);
+//d3.select("#button").on("click", filterTable);
 d3.select("#buttonClear").on("click", clearInputs);
 
 // Attach an event to listen for changes to each filter
-d3.selectAll(".form-control").on("change", updateFilters);
+d3.selectAll(".filter").on("change", updateFilters);
 
-
-
-// if (filteredTable.length === 0) {
-    //     clearBody();
-    //     // If no results found display a message
-    //     d3.select("tbody").html("<h3>No results found</h3>");
-    // } else {
-        //     clearBody();
-        //     console.log(filteredTable);
-        //     table(filteredTable);
-        // }
-        
-        
-        /* let shapes = tableData.map(x=>x.shape);
-        let cities = tableData.map(y=>y.city);
-        
-        function Unique(value, index, self) {
-            return self.indexOf(value) === index;
-        }
-        
-        let uniqueShapes= shapes.filter( Unique );
-        let uniqueCities= cities.filter( Unique );
-        //console.log(uniqueShapes); */
-//console.log(uniqueCities);
